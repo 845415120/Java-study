9f569a83e95a6ef264fbe052c993b3a78074625c
江鹏利 <845415120@qq.com>
2023-08-21 13:26:02 +0800

    第二次提交

--- src/git/demo.java
+++ src/git/demo.java
@@ -4,5 +4,7 @@ public class demo {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello world2");
+
+        System.out.println("dev 01 hello world2");
     }
 }
